5cc514f7b8f4ee6a774a64ce510fb2fe3aa60c68
Maryam Rostami <MRostami@shayan.co>
2021-09-12 18:22:04 +0430

    insert utils

---
+++ shayan-utils/js/apiServices.js
@@ -0,0 +1,56 @@
+   export const apiSerivces = {
+        baseUrl(baseUrl) {
+            this._baseUrl = baseUrl;
+            console.log("baseUrl", this);
+            return this;
+        },
+
+        path(urlPath) {
+            this._path = urlPath;
+            console.log("path", this);
+            return this;
+        },
+
+        method(methodName) {
+            this._method = methodName;
+            return this;
+        },
+
+        data(params) {
+            this._data = params;
+            return this;
+        },
+
+        request(onSuccess, onError) {
+            const link = this._baseUrl + this._path;
+            let requestHeaders = {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            };
+            // let oldToken = this.getToken();
+            // if (oldToken != null && oldToken !== undefined) {
+            //     requestHeaders["Authorization"] = `${oldToken}`;
+            // }
+            const requestMethod = this._method || "GET";
+            let requestProps = {
+                method: requestMethod,
+                headers: requestHeaders,
+                json: true,
+            };
+            if (requestMethod !== "GET") {
+                requestProps.body = JSON.stringify(this._data || {});
+            }
+            return fetch(link, requestProps)
+                .then((res) => res.json())
+                .then((result) => {
+                    onSuccess(result, this._data);
+                })
+                .catch((error) => {
+                    if (onError && typeof onError === "function") {
+                        onError(error, this._data);
+                    }
+                });
+        },
+    };
+
+

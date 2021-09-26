104c066b2611c514c781ed2eeddd14cfd5bcc49e
Maryam Rostami <MRostami@shayan.co>
2021-09-26 09:53:11 +0330

    bug fix

--- shayan-utils/js/pathParser.js
+++
@@ -1,8 +0,0 @@
-export const pathParser = ({ pathname, params }) => {
-  let result = pathname
-  for (const param of Object.keys(params)) {
-    let regex = new RegExp(`:${param}`, "i")
-    result = result.replace(regex, params[param])
-  }
-  return result
-}
